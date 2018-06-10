c5eb951ec5a45c1a44064034b6ca69f058a7a3a5
Evan Miller <evanhumanist@gmail.com>
2018-06-10 11:16:51 -0400

    style: Added comments, standard formatting changes

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-const cardsContent = document.querySelectorAll('.card > i'); //Targets the content of the cards
+const CARDS_CONTENT = document.querySelectorAll('.card > i'); //Targets the content of the cards
 const CARD = document.querySelectorAll('.card');
-const winContent = document.querySelector('.win');
+const WIN_CONTENT = document.querySelector('.win');
 let deckArray = ['fa-gem', 'fa-paper-plane', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb', 'fa-gem', 'fa-paper-plane', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb']; //Our array of card items
-const deck = document.querySelector('.deck');
+const DECK = document.querySelector('.deck');
 const HEADING = document.querySelector('.header-container');
 const SCORE_PANEL = document.querySelector('.score-panel');
 const MOVES = document.querySelector('.moves');
@@ -43,11 +32,13 @@ function shuffle(array) {
     return array;
 }
 
+//Increments the move counter by 1
 function increaseMoveCounter() {
     moveCounter += 1;
     MOVES.textContent = moveCounter.toString();
 }
 
+//Decreases stars depending on the amount of moves
 function starCheck() {
     if (moveCounter === 12) {
         STARS[2].classList.toggle('fas');
@@ -60,7 +51,21 @@ function starCheck() {
     }
 }
 
-//Card match function
+//Displays the Win screen
+function win() {
+    WIN_MOVES.textContent = moveCounter.toString();
+    if (stars === 1) {
+        WIN_STARS.textContent = "1 star";
+    } else {
+        WIN_STARS.textContent = stars + " stars";
+    };
+    DECK.classList.toggle('hide');
+    HEADING.classList.toggle('hide');
+    SCORE_PANEL.classList.toggle('hide');
+    WIN_CONTENT.classList.toggle('hide');
+}
+
+//React to cards being clicked, match/mismatch/win
 function cardClicked(evt) {
     if (evt.target.nodeName === 'LI' && ! evt.target.classList.contains('open') && ! evt.target.classList.contains('match') && cardFlipping === false) {
         cardFlipping = true;
@@ -74,6 +79,8 @@ function cardClicked(evt) {
         } else {
             increaseMoveCounter();
             starCheck();
+
+            //found a match
             if (evt.target.firstElementChild.classList.toString() === firstCard.firstElementChild.classList.toString()) {
                 evt.target.classList.replace('closed', 'open');
                 setTimeout(function () {
@@ -87,21 +94,11 @@ function cardClicked(evt) {
                     matches += 1;
                     if (matches === deckArray.length / 2 ) {
                         setTimeout(function () {
-                            WIN_MOVES.textContent = moveCounter.toString();
-                            if (stars === 1) {
-                                WIN_STARS.textContent = "1 star";
-                            } else {
-                                WIN_STARS.textContent = stars + " stars";
-                            };
-                            deck.classList.toggle('hide');
-                            HEADING.classList.toggle('hide');
-                            SCORE_PANEL.classList.toggle('hide');
-                            winContent.classList.toggle('hide');
+                            win();
                         }, 700);
-                    }
-                    ;
+                    };
                 }, 500);
-            } else {
+            } else { //Not a match
                 evt.target.classList.replace('closed', 'open');
                 setTimeout(function () {
                     evt.target.classList.toggle('show');
@@ -123,44 +120,36 @@ function cardClicked(evt) {
     };
 }
 
+//Starts/restarts the game
 function restart() {
     if (cardFlipping === false) {
         //Shuffles the deck and initializes the items on the cards
         deckArray = shuffle(deckArray);
         for(const index in deckArray) {
             CARD[index].classList = "card closed";
-            cardsContent[index].classList = "fas";
-            cardsContent[index].classList.toggle(deckArray[index]);
+            CARDS_CONTENT[index].classList = "fas";
+            CARDS_CONTENT[index].classList.toggle(deckArray[index]);
             stars = 3;
             STARS[1].classList = 'fas fa-star';
             STARS[2].classList = 'fas fa-star';
             moveCounter = 0;
             matches = 0;
             MOVES.textContent = 0;
-            deck.classList.toggle('hide', false);
+            DECK.classList.toggle('hide', false);
             HEADING.classList.toggle('hide', false);
             SCORE_PANEL.classList.toggle('hide', false);
-            winContent.classList.toggle('hide', true);
+            WIN_CONTENT.classList.toggle('hide', true);
         };
     };
 
 };
 
+//Runs the restart function when the code first runs
 restart();
 
 //Flips cards when clicked
-deck.addEventListener('click', cardClicked);
+DECK.addEventListener('click', cardClicked);
 
+//Runs the restart code when clicked
 RESTART.addEventListener('click', restart);
 WIN_BUTTON.addEventListener('click', restart);
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
